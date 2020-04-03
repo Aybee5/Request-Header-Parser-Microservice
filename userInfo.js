4a53da0a6a2078942bc7a0bd0c55ba028de06c45
Ibrahim Abdullahi Aliyu <ibrahiimaa05@gmail.com>
2020-04-04 00:13:52 +0100

    userInfo handler

---
+++ userInfo.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.get('/api/whoami', (req, res)=>{
+    var ip = (req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress || 
+    '').split(',')[0].trim();
+    var preferredLanguage = req.headers['accept-language']
+    var sysInfo = req.headers['user-agent']
+    res.status(200).json({ipaddress: ip, Language: preferredLanguage, software: sysInfo})
+})
+module.exports = app;
