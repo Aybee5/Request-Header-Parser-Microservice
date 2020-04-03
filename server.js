bb2a979af69fa6d0a3de4a4b91ad6aa8b7a52d59
Ibrahim Abdullahi Aliyu <ibrahiimaa05@gmail.com>
2020-04-04 00:09:02 +0100

    created userInfo handler

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 
+var userInfo = require('./userInfo')
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,7 +25,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.use(userInfo)
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
